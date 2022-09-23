ab8510a1476cb01cf14e6314e1b7fddb17531fec
Jeff <49373923+JeffWll@users.noreply.github.com>
2022-09-22 23:29:51 -0400

    Mecanismo de login rusticamente inapropriado

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,21 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Login.css';
 import Logo from "../Assets/logo.png";
 import Mail from "../Assets/icon_mail.svg";
 import Lock from "../Assets/icon_lock.svg";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
-var pessoa1 = 'Joao da silva'
-var psw = '1234'
 
-var pessoa1 = 'Lara da silva'
-var psw = '1234'
+const users = [
+  {
+    username: 'admin1',
+    password: '12345678'
+  },
+  {
+    username:'admin2',
+    password:'012345678'
+  }
+];
 
-var pessoa1 = 'Felipe da silva'
-var psw = '1234'
+var data = {
+  username: '',
+  password: ''
+};
 
 
 const Login = () => {
+ 
+  const [message] = useState('');
+
+  const history = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    checkUser();
+    console.log(checkUser());
+  }
+
+  const routeChange = () =>{ 
+    history("./Dashboard");}
+
+  const checkUser = () => {
+    const usercheck = users.find(user => (user.username === data.username && user.password === data.password));
+    if(usercheck) {
+      console.log("Login successful");
+      
+      routeChange();
+
+    }else {
+      console.log("Wrong password or username");
+      alert("Senha Incorreta!");
+      console.log(data.username);
+      console.log(data.password);
+    }
+    // console.log(uname);
+    console.log(usercheck);
+  }
+
+  const handleEmailChange = event => {
+    data.username = event.target.value;
+    console.log("Valor Email Inputado");
+    //console.log(data.username);
+  }
+  const handlePswChange = event => {
+    data.password = event.target.value;
+    console.log("Valor Psw Inputado");
+    //console.log(data.password);
+  }
+  
+
   return (
    <>
     <div className="container text-center mt-5 corpo_login">
@@ -28,16 +80,17 @@ const Login = () => {
           <p>Informe seu Email e Senha</p>
         </div>
         <div className="col-lg-12">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div class="form-group mb-3" className="div_input">
-              <input className="form-control input_form" type="text" placeholder="Email"/>
+              <input className="form-control input_form" type="text" placeholder="Email" onClick={handleEmailChange}/>
               <img src={Mail} alt="" />
             </div>
             <div class="form-group mb-3" className="div_input">
-              <input className="form-control input_form" type="text" placeholder="Senha"/>
+              <input className="form-control input_form" type="text" placeholder="Senha" onClick={handlePswChange}/>
               <img src={Lock} alt="" />
             </div>
-            <Link to={"./Dashboard"} class="btn btn-primary btn_login">Login</Link>
+            <input type="submit" value="Logar" />
+              {/*<Link type="submit" to={"./Dashboard"} class="btn btn-primary btn_login">Login</Link>*/}
           </form>
         </div>
         <div className="col-lg-12 mt-3">
